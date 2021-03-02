646ce671e499df4f3caa95fa14ab40ec4f73461e
RAVI <artiveloper@gmail.com>
2021-03-02 13:01:46 +0900

    feat: add karrotframe

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Navigator, Screen, useNavigator} from 'karrotframe'
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Navigator>
+        <Screen path='/' component={Home} />
+        <Screen path='/detail' component={Detail} />
+      </Navigator>
   );
+}
+
+const Home: React.FC = () => {
+
+  const { push } = useNavigator();
+
+  const onClickButton = () => {
+    push('/detail');
+  }
+
+  return (
+      <div>
+        Home. <button onClick={onClickButton}>다음 페이지</button>
+      </div>
+  )
+}
+
+const Detail: React.FC = () => {
+  return (
+      <div>
+        Detail.
+      </div>
+  )
 }
 
 export default App;
